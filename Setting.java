342bc87ed0928d05b95389b5bc9859e75cea5793
angela97lin <angela97lin@gmail.com>
2014-06-14 15:06:45 -0400

    ew github window is lame

---
+++ Setting.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.*;
+import java.util.*;
+import java.io.*;
+import java.awt.event.*;
+// A GUI program is written as a subclass of Frame - the top-level container  
+// This subclass inherits all properties from Frame, e.g., title, icon, buttons, content-pane
+
+public class Setting extends JPanel {  
+
+
+
+
+
+
+
+
+
+
+    public void draw(Graphics g){
+	//draw pics
+	//Mouse
+
+
+
+    }
+
+
