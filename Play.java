072f555b5c2f4fb02c555b2813d75cb87803284b
angela97lin <angela97lin@gmail.com>
2014-06-14 16:47:27 -0400

    spacing swag

--- Play.java
+++ Play.java
@@ -22,18 +22,42 @@ public class Play{
     }
 
     public void draw(Graphics g){
-	for (int i = 0; i < p.getHand().size();i++){
-	    p.getHand().getCard(i).draw(g,(GamePlay.myWidth/2
-					   -((c.getHand().size()*53)/2))
-					   +i*53,550);
+	int cSpacing = c.getHand().size()*5;
+	int pSpacing = p.getHand().size()*5;
+	if (p.getHand().size() > 25){
+
+	    for (int i = 0; i < p.getHand().size();i++){
+		p.getHand().getCard(i).draw(g,(GamePlay.myWidth/2
+					       -((c.getHand().size()*53)/2))
+					    +i*40,550);
+	    }
 	}
-	for (int i = 0; i < c.getHand().size();i++){
-	    c.getHand().getCard(i).draw(g,(GamePlay.myWidth/2
-					   -((c.getHand().size()*53)/2))
-					+i*53,53);
+	else { 	    
+	    for (int i = 0; i < p.getHand().size();i++){
+		p.getHand().getCard(i).draw(g,(GamePlay.myWidth/2
+					       -((c.getHand().size()*53)/2))
+					    +i*53,550);
+	    }
+
+	}
+
+	if (c.getHand().size() > 25){
+
+	    for (int i = 0; i < c.getHand().size();i++){
+		c.getHand().getCard(i).draw(g,(GamePlay.myWidth/2
+					       -((c.getHand().size()*53)/2))
+					    +i*40,53);
+	    }
+	}
+
+	else {
+
+       for (int i = 0; i < c.getHand().size();i++){
+		c.getHand().getCard(i).draw(g,(GamePlay.myWidth/2
+					       -((c.getHand().size()*53)/2))
+					    +i*53,53);
+       }
 	}
     }
-    
-
 
 }
--- Play.java~
+++ Play.java~
@@ -23,11 +23,13 @@ public class Play{
 
     public void draw(Graphics g){
 	for (int i = 0; i < p.getHand().size();i++){
-	    p.getHand().getCard(i).draw(g,(145+((13 - c.getHand().size()))*53)
+	    p.getHand().getCard(i).draw(g,(GamePlay.myWidth/2
+					   -((c.getHand().size()*53)/2))
 					   +i*53,550);
 	}
 	for (int i = 0; i < c.getHand().size();i++){
-	    c.getHand().getCard(i).draw(g,(145+((13 - c.getHand().size()))*53)
+	    c.getHand().getCard(i).draw(g,(GamePlay.myWidth/2
+					   -((c.getHand().size()*53)/2))
 					+i*53,53);
 	}
     }
